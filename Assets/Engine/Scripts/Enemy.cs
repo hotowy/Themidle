a3f6040df38dab9bd5a63940339db37c1f9f06b4
hotowy <mateuszhotowy@gmail.com>
2025-11-15 22:36:54 +0100

    Refactor Enemy.cs. Add notes.

--- Assets/Engine/Scripts/Enemy.cs
+++ Assets/Engine/Scripts/Enemy.cs
@@ -4,12 +4,6 @@ using UnityEngine.Serialization;
 
 public class Enemy : MonoBehaviour
 {
-    [Header(Utils.Header.Debug)] 
-    [SerializeField]
-    private Vector2 dirToTarget1;
-    
-    [Header(Utils.Header.Separator)] 
-    
     [Header("Movement behaviour:")]
     [SerializeField]
     private float collisionRaycastSpreadAxisX = 0.25f;
@@ -43,45 +37,20 @@ public class Enemy : MonoBehaviour
     
     void FixedUpdate()
     {
-        if (!castleGate) return;
+        if (castleGate)
+        { 
+            MoveTowardsTarget();
+        }
+    }
 
-        // Kierunek w stronę celu
+    private void MoveTowardsTarget()
+    {
         Vector2 dirToTarget = ((Vector2)castleGate.transform.position - rb.position).normalized;
-        dirToTarget1 = dirToTarget;
-
-        // Sprawdź przeszkody przed sobą
-        RaycastHit2D hit = Physics2D.Raycast(rb.position, dirToTarget, collisionRaycastDistance, obstacleMask);
-        //Debug.DrawRay(rb.position,dirToTarget,Color.red);
-        RaycastHit2D hitL = Physics2D.Raycast(rb.position, dirToTarget + Vector2.left * collisionRaycastSpreadAxisX, collisionRaycastDistance, obstacleMask);
-        //Debug.DrawRay(rb.position,dirToTarget + Vector2.left * collisionRaycastSpreadAxisX,Color.red);
-        RaycastHit2D hitR = Physics2D.Raycast(rb.position, dirToTarget + Vector2.right * collisionRaycastSpreadAxisX, collisionRaycastDistance, obstacleMask);
-        //Debug.DrawRay(rb.position,dirToTarget + Vector2.right * collisionRaycastSpreadAxisX,Color.red);
         
-        if (hitL.collider)
-        {
-            // Jeśli coś przed nami — lekko skręć w bok
-            Vector2 avoidDir = Vector2.Perpendicular(hitL.normal).normalized;
-            dirToTarget = (dirToTarget + -avoidDir * avoidDirectionMoveMultiplier).normalized;
-        }
+        // DISABLED FOR NOW // dirToTarget = AdjustDirectionByAvoidingObstacles(dirToTarget);
         
-        if (hitR.collider)
-        {
-            // Jeśli coś przed nami — lekko skręć w bok
-            Vector2 avoidDir = Vector2.Perpendicular(hitR.normal).normalized;
-            dirToTarget = (dirToTarget + avoidDir * avoidDirectionMoveMultiplier).normalized;
-        }
-        
-        if (!hitL.collider && !hitR.collider && hit.collider)
-        {
-            // Jeśli coś przed nami — lekko skręć w bok
-            Vector2 avoidDir = Vector2.Perpendicular(hit.normal).normalized;
-            dirToTarget = (dirToTarget + avoidDir * avoidDirectionMoveMultiplier*2).normalized;
-        }
-
-        // Dodaj siłę w kierunku bramy
         rb.AddForce(dirToTarget * moveForce, ForceMode2D.Force);
-
-        // Ogranicz prędkość, żeby nie przyspieszały w nieskończoność
+        
         if (rb.linearVelocity.magnitude > maxSpeed)
         {
             rb.linearVelocity = rb.linearVelocity.normalized * maxSpeed;
@@ -98,5 +67,35 @@ public class Enemy : MonoBehaviour
         {
             hp -= damageDealt;
         }
+    }
+
+    private Vector2 AdjustDirectionByAvoidingObstacles(Vector2 dirToTarget)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(rb.position, dirToTarget, collisionRaycastDistance, obstacleMask);
+        //Debug.DrawRay(rb.position,dirToTarget,Color.red);
+        RaycastHit2D hitL = Physics2D.Raycast(rb.position, dirToTarget + Vector2.left * collisionRaycastSpreadAxisX, collisionRaycastDistance, obstacleMask);
+        //Debug.DrawRay(rb.position,dirToTarget + Vector2.left * collisionRaycastSpreadAxisX,Color.red);
+        RaycastHit2D hitR = Physics2D.Raycast(rb.position, dirToTarget + Vector2.right * collisionRaycastSpreadAxisX, collisionRaycastDistance, obstacleMask);
+        //Debug.DrawRay(rb.position,dirToTarget + Vector2.right * collisionRaycastSpreadAxisX,Color.red);
+        
+        if (hitL.collider)
+        {
+            Vector2 avoidDir = Vector2.Perpendicular(hitL.normal).normalized;
+            dirToTarget = (dirToTarget + -avoidDir * avoidDirectionMoveMultiplier).normalized;
+        }
+        
+        if (hitR.collider)
+        {
+            Vector2 avoidDir = Vector2.Perpendicular(hitR.normal).normalized;
+            dirToTarget = (dirToTarget + avoidDir * avoidDirectionMoveMultiplier).normalized;
+        }
+        
+        if (!hitL.collider && !hitR.collider && hit.collider)
+        {
+            Vector2 avoidDir = Vector2.Perpendicular(hit.normal).normalized;
+            dirToTarget = (dirToTarget + avoidDir * avoidDirectionMoveMultiplier*2).normalized;
+        }
+
+        return dirToTarget;
     }
 }
